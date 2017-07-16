3961a64607fb38103a2595d7b8c75c4c6a4bf424
brandon <bcooper085@gmail.com>
2017-07-15 17:57:01 -0700

    add firebase to project and link to account

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyAy5n808GrubDUiKTBFdRMTCyOmmRTTFGY",
+    authDomain: "brew-board.firebaseapp.com",
+    databaseURL: "https://brew-board.firebaseio.com",
+    projectId: "brew-board",
+    storageBucket: "brew-board.appspot.com",
+    messagingSenderId: "368770351741"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
